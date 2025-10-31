95a35955fb524981a4856acab5351bb89239a368
Gennaro Prota <gennaro.prota@gmail.com>
2025-10-31 16:09:23 +0100

    Add the unit tests

---
+++ test/test_suite.cpp
@@ -0,0 +1,13 @@
+//
+//  Copyright (c) 2019 Vinnie Falco (vinnie.falco@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/cppalliance/handlebars
+//
+
+// Test that header file is self-contained.
+#include "test_suite.hpp"
+
